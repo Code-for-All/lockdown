5d283bcdcb26a7ed778ce631376c0426e0b9e792
Lars den Bakker <larsdenbakker@gmail.com>
2020-03-21 12:57:16 +0100

    Use custom index.html

--- preact.config.js
+++ preact.config.js
@@ -2,10 +2,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 export default {
   webpack(config, env) {
-    if (env.production) {
-      config.output.publicPath = '/lockdown/';
-      const htmlPlugin = config.plugins.find(plugin => plugin instanceof HtmlWebpackPlugin);
-      htmlPlugin.options.template = require.resolve('./github-pages.index.html');
-    }
+    // if (env.production) {
+    config.output.publicPath = '/lockdown/';
+    // const htmlPlugin = config.plugins.find(plugin => plugin instanceof HtmlWebpackPlugin);
+    // htmlPlugin.options.template = require.resolve('./github-pages.index.html');
+    // }
   }
 };
