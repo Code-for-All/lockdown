4a7e3e1ed648b6bcc3b926b2561d4a03655853a2
Lars den Bakker <larsdenbakker@gmail.com>
2020-03-21 12:50:46 +0100

    Only adjust base href in production

--- preact.config.js
+++ preact.config.js
@@ -1,15 +1,11 @@
-// these props are both optional
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 export default {
-  /**
-   * Function that mutates the original webpack config.
-   * Supports asynchronous changes when a promise is returned (or it's an async function).
-   *
-   * @param {object} config - original webpack config.
-   * @param {object} env - options passed to the CLI.
-   * @param {WebpackConfigHelpers} helpers - object with useful helpers for working with the webpack config.
-   * @param {object} options - this is mainly relevant for plugins (will always be empty in the config), default to an empty object
-   **/
-  webpack(config, env, helpers, options) {
-    config.output.publicPath = '/lockdown/';
+  webpack(config, env) {
+    if (env.production) {
+      config.output.publicPath = '/lockdown/';
+      const htmlPlugin = config.plugins.find(plugin => plugin instanceof HtmlWebpackPlugin);
+      htmlPlugin.options.template = require.resolve('./github-pages.index.html');
+    }
   }
 };
