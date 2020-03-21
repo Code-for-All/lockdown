5fec6e3393c66c0b14c29d6e37f7537924fdd538
Lars den Bakker <larsdenbakker@gmail.com>
2020-03-21 10:59:05 +0100

    Set up map coloring

--- src/components/world-map/index.js
+++ src/components/world-map/index.js
@@ -5,6 +5,7 @@ import mapData from '../../../data/worldmap.json';
 import themeData from '../../../data/datafile.json';
 
 const mapbox_token = 'pk.eyJ1IjoibWlibG9uIiwiYSI6ImNrMGtvajhwaDBsdHQzbm16cGtkcHZlaXUifQ.dJTOE8FJc801TAT0yUhn3g';
+const today = new Date();
 
 class WorldMap extends Component {
   componentDidMount() {
@@ -17,14 +18,17 @@ class WorldMap extends Component {
     });
     let themeLayer;
     let labelLayer = layerGroup();
+
     function zoomToFeature(e) {
       const layer = e.target;
       map.fitBounds(layer.getBounds());
     }
+
     function resetHighlight(e) {
       const layer = e.target;
       themeLayer.resetStyle(layer);
     }
+
     function highlightFeature(e) {
       const layer = e.target;
       layer.setStyle({
@@ -35,6 +39,7 @@ class WorldMap extends Component {
         layer.bringToFront();
       }
     }
+
     function onEachFeature(feature, layer) {
       layer.on({
         mouseover: highlightFeature,
@@ -42,42 +47,51 @@ class WorldMap extends Component {
         click: zoomToFeature
       });
     }
+
     tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token=' + mapbox_token, {
       tileSize: 512,
       zoomOffset: -1,
       attribution:
         '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+
     function worldStyle(e) {
       let value;
       if (e.properties.data && e.properties.data.lockdowns) {
+        if (e.properties.data.lockdowns.length === 0) {
+          value = 'green';
+        }
+
         for (const lockdown of e.properties.data.lockdowns) {
-          // TODO...
-          //Is a lockdown currently in effect?
-          // if (moment(lockdown.start) < moment() && moment(lockdown.end ? lockdown.end : moment().add(365,'day')) > moment()) {
-          // value = "red";
-          // ++activeLockdowns;
-          // $('#active-lockdowns').html(activeLockdowns);
-          // }
+          // TODO: start and end are exclusive or inclusive?
+          if (new Date(lockdown.start) >= today && lockdown.end ? new Date(lockdown.end) < today : true) {
+            value = 'red';
+          } else {
+            value = 'green';
+          }
         }
       }
+
       const style = {
         weight: 1,
         opacity: 0.1,
         color: '#555',
         fillOpacity: 0
       };
+
       if (value) {
         style.fillColor = value;
         style.fillOpacity = 0.5;
       }
       return style;
     }
+
     for (const feature of mapData.features) {
       if (themeData[feature.properties.NAME]) {
         feature.properties.data = themeData[feature.properties.NAME];
       }
     }
+
     themeLayer = geoJSON(mapData, {
       style: worldStyle,
       onEachFeature: onEachFeature
