a204fc92f0c0515a528ffe757526941a95ba63d0
Lars den Bakker <larsdenbakker@gmail.com>
2020-03-21 13:36:16 +0100

    Set custom template only in build with CLI

--- preact.config.js
+++ preact.config.js
@@ -1,11 +1,7 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 export default {
   webpack(config, env) {
-    // if (env.production) {
-    config.output.publicPath = '/lockdown/';
-    // const htmlPlugin = config.plugins.find(plugin => plugin instanceof HtmlWebpackPlugin);
-    // htmlPlugin.options.template = require.resolve('./github-pages.index.html');
-    // }
+    if (env.production) {
+      config.output.publicPath = '/lockdown/';
+    }
   }
 };
