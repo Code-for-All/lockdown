a7a778b3473d97882d75226bab4581bee3e217f0
thepassle <pascalschilp@gmail.com>
2020-03-24 16:39:15 +0100

    refactor: return obj

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,61 +10,66 @@ const refresh = new URL('../assets/icons/refresh.svg', import.meta.url).href;
 const add = new URL('../assets/icons/circle-plus.svg', import.meta.url).href;
 
 const renderMenu = menuItem => {
-  const output = {};
   switch (menuItem) {
     case 'info':
-      output.title = 'info';
-      output.template = html`
-        info
-      `;
-      break;
+      return {
+        title: 'info',
+        template: html`
+          info
+        `
+      };
     case 'settings':
-      output.title = 'settings';
-      output.template = html`
-        settings
-      `;
-      break;
+      return {
+        title: 'settings',
+        template: html`
+          settings
+        `
+      };
     case 'contribute':
-      output.title = 'contribute';
-      output.template = html`
-        <${Expandable}
-          toggle=${'Sources'}
-          detail=${html`
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-              in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          `}
-        />
-        <${Expandable}
-          toggle=${'Credits'}
-          detail=${html`
-            <p>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque
-              ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
-              voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-              Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi
-              tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem
-              ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea
-              voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-            </p>
-          `}
-        />
-      `;
-      break;
+      return {
+        title: 'contribute',
+        template: html`
+          <${Expandable}
+            toggle=${'Sources'}
+            detail=${html`
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            `}
+          />
+          <${Expandable}
+            toggle=${'Credits'}
+            detail=${html`
+              <p>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque
+                ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+                voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+                Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi
+                tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+                exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure
+                reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
+                nulla pariatur?
+              </p>
+            `}
+          />
+        `
+      };
     case 'ticker':
-      output.title = 'ticker';
-      output.template = html`
-        ticker
-      `;
-      break;
+      return {
+        title: 'ticker',
+        template: html`
+          ticker
+        `
+      };
     default:
-      output.title = '';
-      output.template = html``;
+      return {
+        title: '',
+        template: html``
+      };
   }
-  return output;
 };
 
 export class Menu extends Component {
