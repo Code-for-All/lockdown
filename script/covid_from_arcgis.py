8f7f0af1c4fb34b806551f4afd4e9b49605b9630
milovanderlinden <milo@dogodigi.net>
2020-03-24 15:45:10 +0100

    Added totals

--- script/covid_from_arcgis.py
+++ script/covid_from_arcgis.py
@@ -35,16 +35,31 @@ resp = requests.get(url=url, params=params)
 data = resp.json() # Check the JSON Response Content documentation below
 features = data['features']
 result = []
+total_confirmed = 0
+total_death = 0
 for feature in features:
     # if 'ADMO_NAME' in feature['attributes'].keys():
+    total_death += feature['attributes']['cum_death']
+    total_confirmed += feature['attributes']['cum_conf']
     temp = {
         'name': feature['attributes']['ADM0_NAME'],
-        'entered': datetime.datetime.fromtimestamp(feature['attributes']['DateOfDataEntry']/1000).strftime('%Y-%m-%dT%H:%M:00.0Z'),
+        'date': datetime.datetime.fromtimestamp(feature['attributes']['DateOfDataEntry']/1000).strftime('%Y-%m-%dT%H:%M:00.0Z'),
         'confirmed': feature['attributes']['cum_conf'],
         'deaths': feature['attributes']['cum_death']
     }
     result.append(temp)
+
+total = {
+    "date": datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:00.0Z'),
+    "confirmed": total_confirmed,
+    "deaths": total_death
+}
+
 with open('../data/coronastatus.json', 'w') as f:
     json.dump(result, f, skipkeys=True, ensure_ascii=True,
               indent=2, separators=None, default=None, sort_keys=True)
     
+with open('../data/totals.json', 'w') as f:
+    json.dump(total, f, skipkeys=True, ensure_ascii=True,
+              indent=2, separators=None, default=None, sort_keys=True)
+
