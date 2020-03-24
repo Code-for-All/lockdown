e33f9aeb1bda1d1962ba2311cd66d656e83acde7
thepassle <pascalschilp@gmail.com>
2020-03-24 15:34:30 +0100

    refactor: reuse dialog

--- src/components/CountryInfo.js
+++ src/components/CountryInfo.js
@@ -2,16 +2,8 @@ import { html } from 'htm/preact';
 import css from 'csz';
 import { Component } from 'preact';
 
-const KEYCODE_ESC = 27;
-
 const styles = css`
   & {
-    position: fixed;
-    z-index: 1100;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -22,8 +14,6 @@ const styles = css`
     padding: 16px;
     border-radius: 8px;
     width: 100%;
-    max-width: 320px;
-    margin-left: 400px;
   }
 
   h1 {
@@ -45,25 +35,10 @@ const styles = css`
 `;
 
 export class CountryInfo extends Component {
-  constructor() {
-    super();
-
-    this.__onClick = this.__onClick.bind(this);
-    this.__onKeyDown = this.__onKeyDown.bind(this);
-  }
-
-  componentWillMount() {
-    window.addEventListener('keydown', this.__onKeyDown, true);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.__onKeyDown, true);
-  }
-
   render() {
     return html`
-      <div class=${styles} onClick=${this.__onClick}>
-        <div class="dialog" ref=${ref => (this.dialogRef = ref)}>
+      <div class=${styles}>
+        <div class="dialog">
           <h1>${this.props.country}</h1>
 
           <div class="data-entry">Population: <span class="data-value">Unknown</span></div>
@@ -74,18 +49,5 @@ export class CountryInfo extends Component {
         </div>
       </div>
     `;
-  }
-
-  __onClick(e) {
-    const clickedOutside = !e.path.includes(this.dialogRef);
-    if (clickedOutside) {
-      this.props.close();
-    }
-  }
-
-  __onKeyDown(e) {
-    if (e.keyCode === KEYCODE_ESC) {
-      this.props.close();
-    }
   }
 }
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -7,6 +7,8 @@ import { Totals } from '../components/Totals.js';
 import { router } from '../router.js';
 import { Menu } from '../components/Menu.js';
 
+const KEYCODE_ESC = 27;
+
 const totalsStyles = css`
   & {
     position: fixed;
@@ -27,14 +29,57 @@ const totalsStyles = css`
   }
 `;
 
+const dialogStyles = css`
+  & {
+    width: 90%;
+    max-width: 400px;
+    left: calc(50% + 200px);
+    top: 50%;
+    transform: translate(-50%, -50%);
+    height: 70%;
+    position: fixed;
+    z-index: 1100;
+    background-color: white;
+    box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.12), 0 8px 8px 0 rgba(0, 0, 0, 0.24);
+    border-radius: 5px;
+  }
+
+  .ld-dialog--header {
+  }
+
+  .ld-dialog--close {
+  }
+
+  .ld-dialog--content {
+  }
+
+  @media (max-width: 960px) {
+    & {
+      width: 90%;
+      max-width: 400px;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+      height: 70%;
+    }
+  }
+`;
+
 export class MainPage extends Component {
   constructor() {
     super();
     this.state = {
-      location: {}
+      dialog: {
+        opened: false,
+        template: {}
+      }
     };
+
     this.__onPathChanged = this.__onPathChanged.bind(this);
     this.__closeCountryInfo = this.__closeCountryInfo.bind(this);
+    this.__closeDialog = this.__closeDialog.bind(this);
+    this.__change = this.__change.bind(this);
+    this.__onKeyDown = this.__onKeyDown.bind(this);
   }
 
   async componentDidMount() {
@@ -44,10 +89,12 @@ export class MainPage extends Component {
   }
 
   componentWillMount() {
+    window.addEventListener('keydown', this.__onKeyDown, true);
     router.addEventListener('path-changed', this.__onPathChanged);
   }
 
   componentWillUnmount() {
+    window.removeEventListener('keydown', this.__onKeyDown, true);
     router.removeEventListener('path-changed', this.__onPathChanged);
   }
 
@@ -65,27 +112,66 @@ export class MainPage extends Component {
       </div>
 
       <${WorldMap} countriesData=${this.state.countriesData} />
-      <${Menu} />
+      <${Menu} change=${this.__change} close=${this.__closeDialog} />
 
-      ${this.state.selectedCountry
+      ${this.state.dialog.opened
         ? html`
-            <${CountryInfo}
-              country=${this.state.selectedCountry}
-              countryData=${this.state.countriesData[this.state.selectedCountry]}
-              close=${this.__closeCountryInfo}
-            />
+            <div class="${dialogStyles}">
+              <div class="ld-dialog--header">
+                <button onClick=${this.__closeDialog} class="ld-dialog--close">X</button>
+                <h1>hello</h1>
+              </div>
+              <div class="ld-dialog--content">
+                ${this.state.dialog.template}
+              </div>
+            </div>
           `
-        : null}
+        : ''}
     `;
   }
 
   __onPathChanged() {
+    if (router.url.searchParams.get('country')) {
+      this.setState({
+        dialog: {
+          opened: true,
+          template: html`
+            <${CountryInfo}
+              country=${router.url.searchParams.get('country')}
+              countryData=${this.state.countriesData[this.state.selectedCountry]}
+            />
+          `
+        }
+      });
+    }
+  }
+
+  __onKeyDown(e) {
+    if (e.keyCode === KEYCODE_ESC) {
+      this.__closeDialog();
+    }
+  }
+
+  __change(template) {
     this.setState({
-      selectedCountry: router.url.searchParams.get('country')
+      dialog: {
+        opened: true,
+        template
+      }
     });
   }
 
   __closeCountryInfo() {
     router.setSearchParam('country', undefined);
+  }
+
+  __closeDialog() {
+    this.setState({
+      dialog: {
+        opened: false,
+        template: ''
+      }
+    });
+    this.__closeCountryInfo();
   }
 }
