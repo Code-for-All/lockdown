19f9dc9f7123d2ce8e6572c9fdbec84ee05ced09
thepassle <pascalschilp@gmail.com>
2020-03-26 22:33:40 +0100

    fix: remove race condition when fetching totals.json

--- src/services/totalsService.js
+++ src/services/totalsService.js
@@ -3,7 +3,8 @@ import { EventTargetShim } from '../utils/EventTargetShim.js';
 class TotalsService extends EventTargetShim {
   async getTotals(forceRefresh) {
     if (forceRefresh || !this.__totals) {
-      this.__totals = await (await fetch(new URL('../../data/totals.json', import.meta.url))).json();
+      this.__totals = fetch(new URL('../../data/totals.json', import.meta.url));
+      this.__totals = await (await this.__totals).json()
       this.dispatchEvent(new Event('change'));
     }
     return this.__totals;
