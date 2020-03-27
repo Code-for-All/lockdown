384daa35956d7a4df19afee4834ecd1efb2ed445
thepassle <pascalschilp@gmail.com>
2020-03-27 20:19:29 +0100

    feat: initial sw config

--- rollup.config.js
+++ rollup.config.js
@@ -5,46 +5,59 @@ import babel from 'rollup-plugin-babel';
 import copy from 'rollup-plugin-copy';
 import { injectManifest } from 'rollup-plugin-workbox';
 
-export default {
-  input: 'index.html',
-  output: {
-    entryFileNames: '[hash].js',
-    chunkFileNames: '[hash].js',
-    format: 'es',
-    dir: 'build'
+export default [
+  {
+    input: 'sw.js',
+    output: {
+      format: 'es',
+      dir: 'build'
+    },
+    plugins: [
+      resolve(),
+      // terser({ output: { comments: false } }),
+    ]
   },
+  {
+    input: 'index.html',
+    output: {
+      entryFileNames: '[hash].js',
+      chunkFileNames: '[hash].js',
+      format: 'es',
+      dir: 'build'
+    },
 
-  plugins: [
-    resolve(),
-    html(),
-    babel({
-      babelHelpers: 'bundled',
-      presets: [require.resolve('@babel/preset-modules')],
-      plugins: [
-        [require.resolve('babel-plugin-htm'), { import: 'preact' }],
-        [require.resolve('babel-plugin-bundled-import-meta'), { importStyle: 'baseURI' }],
-        require.resolve('@babel/plugin-proposal-optional-chaining'),
-        require.resolve('@babel/plugin-proposal-nullish-coalescing-operator')
-      ]
-    }),
-    terser({ output: { comments: false } }),
-    copy({
-      hook: 'buildStart',
-      targets: [
-        { src: 'src/assets/**/*', dest: 'build/src' },
-        { src: 'src/style/**/*', dest: 'build/src' }
-      ],
-      flatten: false
-    }),
-    copy({
-      hook: 'buildStart',
-      targets: [{ src: 'data/**/*', dest: 'build/data' }],
-      flatten: false
-    }),
-    injectManifest({
-      swSrc: 'sw.js',
-      swDest: '/build/sw.js',
-      globDirectory: 'demo/build/',
-    })
-  ]
-};
+    plugins: [
+      resolve(),
+      html(),
+      babel({
+        babelHelpers: 'bundled',
+        presets: [require.resolve('@babel/preset-modules')],
+        plugins: [
+          [require.resolve('babel-plugin-htm'), { import: 'preact' }],
+          [require.resolve('babel-plugin-bundled-import-meta'), { importStyle: 'baseURI' }],
+          require.resolve('@babel/plugin-proposal-optional-chaining'),
+          require.resolve('@babel/plugin-proposal-nullish-coalescing-operator')
+        ]
+      }),
+      terser({ output: { comments: false } }),
+      copy({
+        hook: 'buildStart',
+        targets: [
+          { src: 'src/assets/**/*', dest: 'build/src' },
+          { src: 'src/style/**/*', dest: 'build/src' }
+        ],
+        flatten: false
+      }),
+      copy({
+        hook: 'buildStart',
+        targets: [{ src: 'data/**/*', dest: 'build/data' }],
+        flatten: false
+      }),
+      injectManifest({
+        swSrc: 'build/sw.js',
+        swDest: 'build/sw.js',
+        globDirectory: 'build/',
+      })
+    ]
+  }
+];
---
+++ sw.js
@@ -0,0 +1,51 @@
+import { registerRoute, NavigationRoute } from 'workbox-routing';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { precacheAndRoute } from 'workbox-precaching';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
+import { createHandlerBoundToURL } from 'workbox-precaching';
+
+/* Cache the Google Fonts stylesheets with a stale-while-revalidate strategy. */
+registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  new StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  })
+);
+
+/* Cache the underlying font files with a cache-first strategy for 1 year. */
+registerRoute(
+  /^https:\/\/fonts\.gstatic\.com/,
+  new CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
+
+/* Precache manifest */
+precacheAndRoute(self.__WB_MANIFEST);
+
+/* Return index.html on navigations */
+const handler = createHandlerBoundToURL('/index.html');
+const navigationRoute = new NavigationRoute(handler, {});
+registerRoute(navigationRoute);
+
+/* 
+TODO:
+  - precache json response? Runtime cache it?
+  - any issues with basepath?
+  - api calls/fetch calls
+    - api.coronatracker.com/
+    - travel-advisory.info/api
+  - dynamic imports
+    - leaflet is dynamically imported
+  - strategy for https://api.mapbox.com/styles/v1/mapbox api requests (6 in total)
+*/
