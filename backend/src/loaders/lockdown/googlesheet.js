55cf4696369ab8a2d200471d24724f9163c4616a
Ilyas Foo <foo.ilyas@gmail.com>
2020-03-28 19:30:34 +0800

    temporarily comment out lockdown loader

--- backend/src/loaders/lockdown/googlesheet.js
+++ backend/src/loaders/lockdown/googlesheet.js
@@ -1,26 +1,27 @@
-import { GoogleSpreadsheet } from 'google-spreadsheet';
-import { lockdownSheetId, googleServiceCredentials } from '../../config';
+// import { GoogleSpreadsheet } from 'google-spreadsheet';
+// import { lockdownSheetId, googleServiceCredentials } from '../../config';
 
-const doc = new GoogleSpreadsheet(lockdownSheetId);
+// const doc = new GoogleSpreadsheet(lockdownSheetId);
 
-/**
- * Initialize google spreadsheet object
- */
-async function init() {
-  await doc.useServiceAccountAuth(googleServiceCredentials);
-  await doc.loadInfo(); // loads document properties and worksheets
-}
+// /**
+//  * Initialize google spreadsheet object
+//  */
+// export async function init() {
+//   await doc.useServiceAccountAuth(googleServiceCredentials);
+//   await doc.loadInfo(); // loads document properties and worksheets
+// }
 
-/**
- * Populate data from 'ASM' sheet
- */
-async function asm() {
-  const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
-  // TODO: figure out structure
-  console.log('Sheet', sheet)
-}
+// /**
+//  * Populate data from 'Territories' sheet
+//  */
+// export async function asm() {
+//   const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
+//   // TODO: figure out structure
+//   return {
+//   };
+// }
 
-export default async function load() {
-  await init();
-  await asm();
-}
+// export default async function load() {
+//   await init();
+//   // await asm();
+// }
