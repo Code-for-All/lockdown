2bb5d6f54354a899ea2c29a8db2fdf6dc6191cb9
Ilyas Foo <foo.ilyas@gmail.com>
2020-03-29 14:06:08 +0800

    improvements to lockdown loader

--- backend/src/loaders/lockdown/lockdown.js
+++ backend/src/loaders/lockdown/lockdown.js
@@ -22,7 +22,30 @@ async function getGlobalData() {
   return transposeRows(headers, rows);
 }
 
-// TODO: Generate entry lists and loop through all the obtained cells to save roundtrips to gsheet
+/**
+ * Parses structure to standardized entry with appended label
+ * @param {array} rows 
+ * @param {array} labels 
+ */
+function parseEntryStructure(rows, labels) {
+  const associativeRows = transposeRows([
+    'start',
+    'end',
+    'value'
+  ], rows);
+
+  const values = transposeColumns(labels, associativeRows);
+  const array = []
+  for (const [label, row] of Object.entries(values)) {
+    array.push({
+      label: label,
+      ...row
+    })
+  }
+  return array;
+}
+
+// TODO: Generate entry lists and loop through all the obtained cells
 async function getDemoData() {
   const sheet = await getWorksheetByTitle('DEMO');
   // Precache cells data for all entries
@@ -32,31 +55,49 @@ async function getDemoData() {
   const entryMetaRows = getCachedCellsRange(sheet, 'H2:J6');
   const entryMetaData = transposeColumns(['editor', 'reviewed_by', 'status', 'type', 'date_of_entry'], entryMetaRows, true);
 
+  // TODO: should skip status != 'Ready'?
+
   // Entry entry section
   const entryInfoRows = getCachedCellsRange(sheet, 'H9:J12');
   const entryInfoData = transposeColumns(['name', 'url', 'title', 'date'], entryInfoRows, true);
 
-  // Other sections: measures, in & out, etc... 
-  const entryOtherRows = getCachedCellsRange(sheet, 'H14:J60');
-  const entryOtherValues = transposeRows([
-    'start',
-    'end',
-    'value'
-  ], entryOtherRows);
+  // Measures section
+  const measures = parseEntryStructure(getCachedCellsRange(sheet, 'H14:J60'), [
+      'max_gathering', // Max gathering number allowed (PAX)?
+      'lockdown_status', // Is there a mandate for self-isolation?
+      'city_movement_restriction', // Is going on the street allowed?
+      'attending_religious_sites', // Is attenting religiouns sites allowed?
+      'going_to_work', // Is going to work allowed?
+      'military_not_deployed', // Is the Military NOT deployed?
+      'academia_allowed', // Is going to academia allowed?
+      'going_to_shops', // Is going to shops allowed?
+      'electricity_nominal', // Is Electricity operating nominally?
+      'water_nominal', // Is Water operating nominally?
+      'internet_nominal', // Is Internet operating nominally?
+  ]);
 
-  const entryOtherData = transposeColumns([
-    'max_gathering',
-    'lockdown_status',
-    'city_movement_restriction',
-    //...
-  ], entryOtherValues);
+  // In & out section
+  const landBordersCrossing = parseEntryStructure(getCachedCellsRange(sheet, 'H32:J38'), [
+      'local', // Local destinations?
+      'nationals_inbound', // Nationals inbound?
+      'nationals_outbound', // Nationals outbound?
+      'foreigners_inbound', // Foreigners inbound?
+      'foreigners_outbound', // Foreigners outbound?
+      'cross_border_workers', // Cross border workers?
+      'commerce', // Commerce?
+  ]);
+
+  // TODO: the rest of in & out
   
   return {
-    ...entryMetaData,
     entries: [
       {
-        ...entryInfoData,
-        ...entryOtherData
+        meta: entryMetaData,
+        info: entryInfoData,
+        measures: measures,
+        in_and_out: [
+          ...landBordersCrossing,
+        ]
       }
     ]
   };
