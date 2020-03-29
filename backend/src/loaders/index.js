4effdf0f41bcbe62c3361b9776dca6d6c1203e3c
Ilyas Foo <foo.ilyas@gmail.com>
2020-03-29 16:14:33 +0800

    feat: added elapsed time logger

--- backend/src/loaders/index.js
+++ backend/src/loaders/index.js
@@ -1,3 +1,4 @@
+import { performance } from 'perf_hooks';
 import lockdown from './lockdown/lockdown';
 // import coronaStatus from './corona/status';
 import logger from '../utils/logger';
@@ -6,9 +7,11 @@ import logger from '../utils/logger';
  * Execute all loaders
  */
 async function executeLoaders() {
+  const t0 = performance.now();
   logger.log('[Lockdown] start');
   await lockdown();
-  logger.log('COMPLETE');
+  const t1 = performance.now();
+  logger.log(`Completed, took ${Math.round(t1 - t0)} milleseconds`);
 }
 
 executeLoaders();
