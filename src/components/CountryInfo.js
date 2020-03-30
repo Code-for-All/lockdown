fb1478cbc43e9a20f4e3d0746e1ef1b21351393d
thepassle <pascalschilp@gmail.com>
2020-03-30 21:39:27 +0200

    chore: remove if statement

--- src/components/CountryInfo.js
+++ src/components/CountryInfo.js
@@ -72,8 +72,7 @@ export class CountryInfo extends Component {
 
   render(_, { lockdowns, travelAdvice, coronaData }) {
     /** If the user is offline, and theres no response, or the response has failed */
-    // if (!navigator.onLine) {
-    if (true) {
+    if (!navigator.onLine) {
       if (travelAdvice?.status !== 'success' || coronaData?.status !== 'success') {
         return html`
           Looks like you're offline :(
