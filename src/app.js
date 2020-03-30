0b1819574d3a557c95960c422b9509f81ad54d39
thepassle <pascalschilp@gmail.com>
2020-03-30 16:48:32 +0200

    chore: improve version log

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { Header } from './components/Header.js';
 import { MainPage } from './pages/MainPage.js';
 
 const version = '1.0.2';
-console.log(version);
+console.log(`🌐 Project Lockdown, version: ${version}`);
 
 window.addEventListener('appinstalled', () => {
   window.location.reload();
