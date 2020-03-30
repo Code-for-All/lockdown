23d3f97124416970752cd79182e338c189b2eae7
thepassle <pascalschilp@gmail.com>
2020-03-30 22:23:49 +0200

    chore: style offline state

--- src/components/CountryInfo.js
+++ src/components/CountryInfo.js
@@ -61,6 +61,17 @@ const styles = css`
   }
 `;
 
+const offlineStyles = css`
+  & {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    flex-direction: column;
+    height: calc(100% - 60px);
+  }
+`;
+
 export class CountryInfo extends Component {
   async componentWillMount() {
     this.setState({
@@ -72,13 +83,17 @@ export class CountryInfo extends Component {
 
   render(_, { lockdowns, travelAdvice, coronaData }) {
     /** If the user is offline, and theres no response, or the response has failed */
-    if (!navigator.onLine) {
-      if (travelAdvice?.status !== 'success' || coronaData?.status !== 'success') {
+    // if (!navigator.onLine) {
+      if(true) {
+      // if (travelAdvice?.status !== 'success' || coronaData?.status !== 'success') {
         return html`
-          Looks like you're offline :(
+          <div class="${offlineStyles}">
+            <b>You are not connected to the internet</b>
+            <p>Information for this country can't be displayed because you are currently offline. Please check your internet connection.</p>
+          </div>
         `;
       }
-    }
+    // }
 
     /** If there is no data available but the user is online, show loading state */
     if (!lockdowns && !travelAdvice && !coronaData && navigator.onLine) {
