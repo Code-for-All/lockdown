4ef638a7789e07fac9114fa594b4fe61e2b22ebf
isunix11 <isunix11@gmail.com>
2020-04-01 04:10:54 +0800

    updated population

--- src/services/populationService.js
+++ src/services/populationService.js
@@ -1,13 +1,49 @@
 import { EventTargetShim } from '../utils/EventTargetShim.js';
 
+// class PopulationService extends EventTargetShim {
+//   async getPopulation(forceRefresh) {
+//     if (forceRefresh || !this.__population) {
+//       this.__population = fetch(new URL('../../data/population.json', import.meta.url)).then(r => r.json());
+//       await this.__population;
+//       this.dispatchEvent(new Event('change'));
+//     }
+//     return this.__population;
+//   }
+// }
+
 class PopulationService extends EventTargetShim {
-  async getPopulation(forceRefresh) {
-    if (forceRefresh || !this.__population) {
-      this.__population = fetch(new URL('../../data/worldpopulation.json', import.meta.url)).then(r => r.json());
-      await this.__population;
-      this.dispatchEvent(new Event('change'));
+  constructor() {
+    super();
+    this.cache = {};
+  }
+
+  async getPopulation(opts) {
+    let { iso2 } = opts;
+    iso2 = encodeURI(iso2);
+
+    if (!/^[a-zA-Z]{2}$/.test(iso2)) {
+      return;
     }
-    return this.__population;
+
+    if (opts.forceRefresh || this.cache[iso2]?.status === 'failed' || !this.cache[iso2]) {
+      try {
+        this.cache[iso2] = {};
+        const res = await fetch(new URL('../../data/population.json', import.meta.url)).then((r) => r.json());
+        this.cache[iso2] = {
+          status: 'success',
+          totalPopulation: res.data[iso2].Population ?? 0,
+        };
+
+        return this.cache[iso2];
+      } catch (_) {
+        this.cache[iso2] = {
+          status: 'failed',
+        };
+      }
+      this.dispatchEvent(new Event('change'));
+      return this.cache[iso2];
+    }
+    return this.cache[iso2];
   }
 }
 
