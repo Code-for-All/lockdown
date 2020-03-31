a016c6c6175ab32a444da79cda19b8bd704966a7
Ilyas Foo <foo.ilyas@gmail.com>
2020-03-31 12:30:03 +0800

    feat: add frontend & backend shared constants

---
+++ shared/enums.js
@@ -0,0 +1,15 @@
+module.exports = {
+  MEASURES: {
+    NO: 'no',
+    YES: 'yes',
+    PARTIAL: 'partial',
+    UNCLEAR: 'unclear',
+  },
+  TRAVEL: {
+    PARTIAL: 'partial',
+    UNCLEAR: 'unclear',
+    NA: 'n/a',
+    PROHIBITED: 'prohibited',
+    ALLOWED: 'allowed',
+  }
+}
