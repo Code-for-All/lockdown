e3dd2c2fb4ba31c649f8106b7f2f9db21cda8056
thepassle <pascalschilp@gmail.com>
2020-04-01 08:04:07 +0200

    chore: add noopener noreferrer

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -61,7 +61,7 @@ const renderMenu = menuItem => {
             detail=${html`
               <p>
                 <b>Project Lockdown</b> is a Civic Tech initiative from a number of organizations and individuals. You can find the list of
-                project members <a target="_blank" href="https://www.TIOF.Click/LockdownData">here</a> under the <b>Credits</b> tab.
+                project members <a rel="noopener noreferrer" target="_blank" href="https://www.TIOF.Click/LockdownData">here</a> under the <b>Credits</b> tab.
               </p>
             `}
           />
