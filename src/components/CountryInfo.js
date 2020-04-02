455b562553c7680e6d4ec69dba20ccd246d894eb
thepassle <pascalschilp@gmail.com>
2020-04-02 22:11:14 +0200

    chore: use lars' spinner

--- src/components/CountryInfo.js
+++ src/components/CountryInfo.js
@@ -92,22 +92,64 @@ const loadingStyles = css`
 
   svg {
     width: 120px;
-    margin-bottom: 20px;
-    animation: loading-spin infinite 1.5s linear;
+    height: 120px;
+    animation: rotate 2000ms linear infinite;
+    transform-origin: center center;
+    margin: auto;
   }
 
-  @keyframes loading-spin {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
+  circle {
+    stroke-dasharray: 85, 200;
+    /* 0px is requires for edge 15 and lower */
+    stroke-dashoffset: 0px;
+    animation: dash 2000ms ease-in-out infinite;
+    stroke-linecap: round;
+    stroke-width: var(--spinner-stroke-width, 4px);
+    stroke-miterlimit: 10;
+    fill: none;
+    stroke: #828282;
+  }
+
+  @keyframes rotate {
+    100% {
       transform: rotate(360deg);
+    }
+  }
+
+  @keyframes dash {
+    0% {
+      stroke-dasharray: 1, 200;
+      /* 0px is requires for edge 15 and lower */
+      stroke-dashoffset: 0px;
+    }
+    50% {
+      stroke-dasharray: 89, 200;
+      stroke-dashoffset: -35px;
+    }
+    100% {
+      stroke-dasharray: 89, 200;
+      stroke-dashoffset: -124px;
+    }
+  }
+
+  /* Animating SVG does not work on IE11. Use a fallback animation. */
+  @media screen and (-ms-high-contrast: active), (-ms-high-contrast: none) {
+    svg {
+      animation-duration: 1500ms;
+    }
+
+    circle {
+      stroke-linecap: square;
     }
   }
 
   @media (prefers-reduced-motion: reduce) {
     svg {
-      animation: loading-spin infinite 20s linear;
+      animation-duration: 20000ms;
+    }
+
+    circle {
+      animation: dash 20000ms ease-in-out infinite;
     }
   }
 `;
