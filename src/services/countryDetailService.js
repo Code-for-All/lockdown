919ff85c04c0deb28715607359d79696113dd54c
Vadim Kupovykh <kup.vadim@gmail.com>
2020-04-26 17:07:28 +0200

    Format

--- src/services/countryDetailService.js
+++ src/services/countryDetailService.js
@@ -22,45 +22,43 @@ class CountryDetailService extends EventTargetShim {
         const res = await (await fetch(new URL(`../../data/territories/${iso2}.json`, import.meta.url))).json();
 
         this.cache[cacheKey] = res;
-
       } catch (_) {
         this.cache[cacheKey] = {
           status: 'failed',
         };
       }
     }
-      var data = this.cache[cacheKey];
-      if (data.status === "failed") {
-        this.dispatchEvent(new Event('change'));
-        return data;
-      }
+    var data = this.cache[cacheKey];
+    if (data.status === 'failed') {
+      this.dispatchEvent(new Event('change'));
+      return data;
+    }
 
-      const travel = {};
-      const mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(date)
-      var dateFormatted =  `${date.getFullYear()}-${mo}-${date.getDate()}`;
+    const travel = {};
+    const mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(date);
+    var dateFormatted = `${date.getFullYear()}-${mo}-${date.getDate()}`;
 
-      var res = data[dateFormatted];
-      for (const type of Object.keys(res.lockdown.travel)) {
-        for (const { label, value } of res.lockdown.travel[type]) {
-          if (Array.isArray(travel[label])) {
-            travel[label].push(value);
-          } else {
-            travel[label] = [value];
-          }
+    var res = data[dateFormatted];
+    for (const type of Object.keys(res.lockdown.travel)) {
+      for (const { label, value } of res.lockdown.travel[type]) {
+        if (Array.isArray(travel[label])) {
+          travel[label].push(value);
+        } else {
+          travel[label] = [value];
         }
       }
+    }
 
-      var result = {
-        status: 'success',
-        date: res.lockdown.date,
-        measures: res.lockdown.measures,
-        travel,
-        max_gathering: res.lockdown.max_gathering,
-      };
-      this.__lastUpdate = Date.now();
-      this.dispatchEvent(new Event('change'));
-      return result;
-    
+    var result = {
+      status: 'success',
+      date: res.lockdown.date,
+      measures: res.lockdown.measures,
+      travel,
+      max_gathering: res.lockdown.max_gathering,
+    };
+    this.__lastUpdate = Date.now();
+    this.dispatchEvent(new Event('change'));
+    return result;
   }
 }
 
