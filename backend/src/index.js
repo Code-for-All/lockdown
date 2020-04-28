a9029c88b529c4f0d68e98dd8bbb98e728e103e9
Chioma Nzeduru <con241@nyu.edu>
2020-04-28 11:10:00 -0400

    Environment variables setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,2 +1,8 @@
 // Entrypoint
 import './main';
+
+require('dotenv').configure()
+
+console.log(process.env.MONGO_DB_USER)
+console.log(process.env.MONGO_DB_PASSWORD)
+console.log(process.env.MONGO_DB)
