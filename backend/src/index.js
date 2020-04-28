fbe5617f9af0aa9f4ee0728b54af6d68eaf5dc02
Vadim Kupovykh <kup.vadim@gmail.com>
2020-04-28 20:18:28 +0200

    [Del] Removed dotenv

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,2 @@
 // Entrypoint
 import './main';
-
-require('dotenv').configure()
-
-console.log(process.env.MONGO_DB_USER)
-console.log(process.env.MONGO_DB_PASSWORD)
-console.log(process.env.MONGO_DB)
