32c1ad578b53abfed561470f47a4b899ba44cd1b
merchantweb <merchantweb@users.noreply.github.com>
2020-05-10 22:26:22 -0400

    Update TimeSlider - Tooltip FIrefox fix
    In Firefox the tooltip for sliding date would break up when sliding to the right. Fixed by adding 'width: max-content'

--- src/components/TimeSlider.js
+++ src/components/TimeSlider.js
@@ -296,6 +296,7 @@ const sliderSelector = css`
     position: absolute;
     @media (max-width: 960px) {
       top: -27px;
+      padding: 0 40px 0 35px;
     }
     top: -27px;
     left: 24.5%;
@@ -305,6 +306,7 @@ const sliderSelector = css`
     background: transparent;
     font-size: 1rem;
     padding: 0 29px 0 21px;
+    width: max-content;
     & span {
       border: 0px solid #8c8c8c;
       border-radius: 30px;
