d014d61c4acbea85241ab7c8f7114773e4f40118
Vadim Kupovykh <kup.vadim@gmail.com>
2020-05-16 20:12:25 +0200

    Formatting

--- backend/src/loaders/lockdown/lockdown.js
+++ backend/src/loaders/lockdown/lockdown.js
@@ -80,7 +80,6 @@ export async function batchGetTerritoriesEntryData(territories, database) {
         if (insertResult.find(r => r.result.nUpserted == 1)) {
           shouldResetApiCache = true;
         }
-
       }
 
       result.push({
