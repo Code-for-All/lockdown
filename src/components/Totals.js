6fedcc0cb7263dbaf15f31052cf7eb2130b7a369
MarkDatysgeld <45890099+mwd@users.noreply.github.com>
2020-06-15 14:50:26 -0300

    Update Totals.js

--- src/components/Totals.js
+++ src/components/Totals.js
@@ -78,22 +78,22 @@ export class Totals extends Component {
       totals: totals,
     });
   }
-{
-    description: i18n.t('header.totals.territoriesLockdown'),
-    value: Number(totals.territories?.lockdown || 0).toLocaleString(),
-  },
-  {
-    description: i18n.t('header.totals.peopleAffected'),
-    value: Number(totals.territories?.affected || 0).toLocaleString(),
-  },
-  {
-    description: i18n.t('header.totals.cases'),
-    value: Number(totals.corona?.confirmed || 0).toLocaleString(),
-  },
-  {
-    description: i18n.t('header.totals.deaths'),
-    value: Number(totals.corona?.deaths || 0).toLocaleString(),
 
+  //   description: i18n.t('header.totals.territoriesLockdown'),
+  //   value: Number(totals.territories?.lockdown || 0).toLocaleString(),
+  // },
+  // {
+  //   description: i18n.t('header.totals.peopleAffected'),
+  //   value: Number(totals.territories?.affected || 0).toLocaleString(),
+  // },
+  // {
+  //   description: i18n.t('header.totals.cases'),
+  //   value: Number(totals.corona?.confirmed || 0).toLocaleString(),
+  // },
+  // {
+  //   description: i18n.t('header.totals.deaths'),
+  //   value: Number(totals.corona?.deaths || 0).toLocaleString(),
+  //
   render({ selectedDate, i18n }, { totals, desktop }) {
     const items = [
       {
