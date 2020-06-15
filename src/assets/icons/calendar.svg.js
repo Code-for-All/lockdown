fc554580aa3e8a9d2dfa0d81d315c45f57c65081
MarkDatysgeld <45890099+mwd@users.noreply.github.com>
2020-06-15 01:41:58 -0300

    icon

--- src/assets/icons/calendar.svg.js
+++ src/assets/icons/calendar.svg.js
@@ -1,7 +1,6 @@
 import { html } from 'htm/preact';
 
-export const calendar = html`<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 viewBox="0 0 32 32" style="enable-background:new 0 0 32 32;" xml:space="preserve">
+export const calendar = html`<svg version="1.0" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="13" height="14" viewBox="0 0 13 14" style="enable-background:new 0 0 13 14;" xml:space="preserve">
 <style type="text/css">
 	.st0{fill:#FF5C00;}
 	.st1{fill:#333333;}
