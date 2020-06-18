a85a1a9b5c4ddd8c29b7f49b0c61b11a180e2b4a
MarkDatysgeld <45890099+mwd@users.noreply.github.com>
2020-06-18 06:07:59 -0300

    test

--- src/components/pwa-install-button.js
+++ src/components/pwa-install-button.js
@@ -1,47 +1,47 @@
-// let deferredPrompt;
-// let installable;
-//
-// window.addEventListener('beforeinstallprompt', (e) => {
-//   e.preventDefault();
-//   installable = true;
-//   deferredPrompt = e;
-// });
-//
-// class PwaInstallButton extends HTMLElement {
-//   constructor() {
-//     super();
-//     const shadow = this.attachShadow({ mode: 'open' });
-//     shadow.innerHTML = `<slot><button>Install</button></slot>`;
-//   }
-//
-//   connectedCallback() {
-//     this.setAttribute('hidden', '');
-//
-//     this.addEventListener('click', this._handlePrompt.bind(this));
-//     window.addEventListener('beforeinstallprompt', (e) => {
-//       e.preventDefault();
-//       deferredPrompt = e;
-//       this.removeAttribute('hidden');
-//       this.dispatchEvent(new CustomEvent('pwa-installable', { detail: true }));
-//     });
-//
-//     if (installable) {
-//       this.removeAttribute('hidden');
-//     }
-//   }
-//
-//   async _handlePrompt(e) {
-//     e.preventDefault();
-//     deferredPrompt.prompt();
-//     const { outcome } = await deferredPrompt.userChoice;
-//     if (outcome === 'accepted') {
-//       this.dispatchEvent(new CustomEvent('pwa-installed', { detail: true }));
-//       this.setAttribute('hidden', '');
-//       deferredPrompt = null;
-//     } else {
-//       this.dispatchEvent(new CustomEvent('pwa-installed', { detail: false }));
-//     }
-//   }
-// }
-//
-// customElements.define('pwa-install-button', PwaInstallButton);
+let deferredPrompt;
+let installable;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  installable = true;
+  deferredPrompt = e;
+});
+
+class PwaInstallButton extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.innerHTML = `<slot><button>Install</button></slot>`;
+  }
+
+  connectedCallback() {
+    this.setAttribute('hidden', '');
+
+    this.addEventListener('click', this._handlePrompt.bind(this));
+    window.addEventListener('beforeinstallprompt', (e) => {
+      e.preventDefault();
+      deferredPrompt = e;
+      this.removeAttribute('hidden');
+      this.dispatchEvent(new CustomEvent('pwa-installable', { detail: true }));
+    });
+
+    if (installable) {
+      this.removeAttribute('hidden');
+    }
+  }
+
+  async _handlePrompt(e) {
+    e.preventDefault();
+    deferredPrompt.prompt();
+    const { outcome } = await deferredPrompt.userChoice;
+    if (outcome === 'accepted') {
+      this.dispatchEvent(new CustomEvent('pwa-installed', { detail: true }));
+      this.setAttribute('hidden', '');
+      deferredPrompt = null;
+    } else {
+      this.dispatchEvent(new CustomEvent('pwa-installed', { detail: false }));
+    }
+  }
+}
+
+customElements.define('pwa-install-button', PwaInstallButton);
